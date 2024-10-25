f718fdaefecbb21bca4ec9040080be95e0f61d42
Daniel-Gonzalez-Calcines <dgc2211gc@gmail.com>
2024-10-25 12:15:51 +0200

    terminado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export * from './components';
+export { Myfavourite } from './components/Myfavourite';
+export { Myfavouriteborder } from './components/MyfavouriteBorder';
+export { DanielGC } from './components/DanielGC';
+export { CustomButton } from './components/button';
+export { Mybutton } from './components/mybutton';
